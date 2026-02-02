820d0d239a09765c78a9e67fae548762212c4454
jardelna <jardel.a@escolar.ifrn.edu.br>
2026-02-02 07:10:13 -0300

    Front End excluido

--- src/main/resources/static/js/config.js
+++
@@ -1,8 +0,0 @@
-const CONFIG = {
-    // Certifique-se de que essa URL é a mesma onde seu Spring Boot está rodando
-    API_BASE_URL: "http://localhost:8080", 
-    APP_NAME: "Figurama",
-    VERSION: "2.0.0"
-};
-
-window.CONFIG = CONFIG;
