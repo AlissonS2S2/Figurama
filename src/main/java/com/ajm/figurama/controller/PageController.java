4d51b20db80fee0f3d88fb4534cd1413f3d103ff
jardelna <jardel.a@escolar.ifrn.edu.br>
2026-02-02 01:38:36 -0300

    Corringindo erros 2

--- src/main/java/com/ajm/figurama/controller/PageController.java
+++ src/main/java/com/ajm/figurama/controller/PageController.java
@@ -6,11 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class PageController {
 
-    @GetMapping("/")
-    public String index() {
-        return "index";
-    }
-
     @GetMapping("/login")
     public String login() {
         return "login";
